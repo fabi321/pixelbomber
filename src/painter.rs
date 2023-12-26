678d0032e379dc6214d1efe8cce736adb632e7e9
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-12-26 23:45:28 +0100

    even simpler painter

--- src/painter.rs
+++ src/painter.rs
@@ -4,16 +4,14 @@ use crate::{client::Client, image_handler::CommandLib};
 
 /// Paint an image to the canvas, can receive image ids to change between frames of an animation
 pub fn painter(command_lib: CommandLib, rx: Receiver<usize>, mut client: Client) -> Result<()> {
-    let mut current = 0;
-    let mut current_commands = &command_lib[current];
+    let mut current_commands = &command_lib[0];
     // loop over frames
     loop {
         // loop over drawings of a single frame
         client.send_pixel(current_commands)?;
         client.flush()?;
         if let Ok(id) = rx.try_recv() {
-            current = id;
-            current_commands = &command_lib[current];
+            current_commands = &command_lib[id];
         }
     }
 }
