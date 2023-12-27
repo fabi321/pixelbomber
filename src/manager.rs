1ff6200a3f12f5c23c59e17c06f8f9629092a114
Fabian Wunsch <fabian@uriah.heep.sax.de>
2023-12-27 12:45:07 +0100

    properly exit if worker threads stopped

--- src/manager.rs
+++ src/manager.rs
@@ -8,12 +8,15 @@ use pixelbomber::{image_handler::CommandLib, painter, Client};
 
 pub fn manage(commands: CommandLib, threads: u32, host: String, fps: f32) {
     let mut handles = Vec::new();
+    let mut thread_handles = Vec::new();
     println!("Starting threads");
     for _ in 0..threads {
         let client = Client::connect(&host).expect("Could not connect to host");
         let commands_cloned = commands.clone();
         let (tx, rx) = channel();
-        thread::spawn(|| painter(commands_cloned, rx, client).unwrap());
+        thread_handles.push(thread::spawn(|| {
+            let _ = painter(commands_cloned, rx, client);
+        }));
         handles.push(tx);
     }
     if commands.len() > 1 {
@@ -24,10 +27,14 @@ pub fn manage(commands: CommandLib, threads: u32, host: String, fps: f32) {
                 }
                 sleep(Duration::from_secs_f32(1.0 / fps))
             }
+            thread_handles.retain(|v| !v.is_finished());
+            if thread_handles.len() == 0 {
+                break
+            }
         }
     } else {
-        loop {
-            sleep(Duration::from_secs(10))
+        for handle in thread_handles {
+            let _ = handle.join();
         }
     }
 }
